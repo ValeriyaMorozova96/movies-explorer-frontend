40bb3a034e6da417a49b5de506ba07df36a688f3
Morozova Valeriya <sandy.lera@yandex.ru>
2023-07-11 16:37:41 +0300

    верстка основной страницы с инфо о проекте

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,22 +1,16 @@
 import './Navigation.css'
-import { Routes, Route, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
    return (
-      <Routes>
-         <div className="navigation">
-            <Route path="/sign-up"
-               element={<Link to="/sign-up" className="navigation__signup">Регистрация</Link>}
-            />
-            <Route path="/sign-in"
-            element={<Link to="/signin">
+      <div className="navigation">
+            <NavLink to="/sign-up" className="navigation__signup">Регистрация</NavLink>
+            <NavLink to="/signin">
                <button className="navigation__signin">
                   Войти
                </button>
-            </Link>}
-            />
-         </div>
-      </Routes>
+            </NavLink>
+             </div>
    )
 }
 
